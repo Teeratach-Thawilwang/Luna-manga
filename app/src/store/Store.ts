da00b4e858142e42b64cf90cd671b773f25d6d0f
ธีรธัช ถวิลหวัง <100046376+Teeratach-Thawilwang@users.noreply.github.com>
2024-07-30 06:10:18 +0700

    Disable Redux devTools on production.

--- app/src/store/Store.ts
+++ app/src/store/Store.ts
@@ -1,19 +1,20 @@
+import { configureStore } from "@reduxjs/toolkit";
+
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
-import { configureStore } from "@reduxjs/toolkit";
 import backofficeReducer from "@store/BackofficeReducers";
 import frontsideReducer from "@store/FrontSideReducers";
 
 const store = configureStore({
-    reducer: {
-        frontside: frontsideReducer,
-        backoffice: backofficeReducer,
-    },
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware({
-            serializableCheck: false,
-        }),
-    devTools: true,
+  reducer: {
+    frontside: frontsideReducer,
+    backoffice: backofficeReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+  devTools: import.meta.env.MODE !== "production",
 });
 
 export type RootStateType = ReturnType<typeof store.getState>;
