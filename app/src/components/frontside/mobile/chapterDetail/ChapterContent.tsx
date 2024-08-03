916cc50b9e0a7ad5061c02ba36a675f65312746e
ธีรธัช ถวิลหวัง <100046376+Teeratach-Thawilwang@users.noreply.github.com>
2024-08-03 19:36:50 +0700

    Create lazy load image.

--- app/src/components/frontside/mobile/chapterDetail/ChapterContent.tsx
+++ app/src/components/frontside/mobile/chapterDetail/ChapterContent.tsx
@@ -3,12 +3,10 @@
 import styled from "styled-components";
 
 import ChapterHeader from "@components/frontside/mobile/chapterDetail/ChapterHeader";
-import RenderAudio from "@components/frontside/mobile/chapterDetail/RenderAudio";
-import RenderText from "@components/frontside/mobile/chapterDetail/RenderText";
+import ChapterNodes from "@components/frontside/mobile/chapterDetail/ChapterNodes";
 import RequestAudioUsage from "@components/frontside/mobile/chapterDetail/RequestAudioUsage";
 import { ImageResponsiveEnum } from "@enums/ImageResponsiveEnum";
 import { ElementTypeEnum } from "@enums/SlateEditorEnum";
-import { MangaEditorElement, TextEditorElement } from "@interfaces/EditorInterface";
 import ChapterService from "@services/frontside/ChapterService";
 import { loadFontForNovel } from "@utils/Helpers";
 import { box } from "@utils/Themes";
@@ -38,7 +36,7 @@ export default function ChapterContent() {
     <Box>
       <ChapterHeader namePosition="Top" />
       {isAudioNodeExist ? <RequestAudioUsage /> : null}
-      <Wrap>{createContent(nodes, isAudioAccept)}</Wrap>
+      <ChapterNodes nodes={nodes} isAudioAccept={isAudioAccept} />
       <ChapterHeader />
     </Box>
   );
@@ -51,38 +49,3 @@ const Box = styled.div`
 
   margin-top: ${(props) => box(props).space.lg};
 `;
-
-const Wrap = styled.div`
-  /* border: 1px solid red; */
-  box-sizing: border-box;
-  width: 100%;
-  margin: ${(props) => box(props).space.xs} 0;
-`;
-
-const Image = styled.img`
-  /* border: 1px solid red; */
-  width: 100%;
-  display: block;
-`;
-
-function createContent(nodes: TextEditorElement[] | MangaEditorElement[], isAudioAccept: boolean) {
-  return nodes.map((node, key) => {
-    return createRenderNode(node, key, isAudioAccept);
-  });
-}
-
-function createRenderNode(node: TextEditorElement | MangaEditorElement, key: number, isAudioAccept: boolean) {
-  const type = node.type;
-
-  switch (type) {
-    case ElementTypeEnum.AUDIO:
-      if (isAudioAccept) {
-        return <RenderAudio url={node.url!} sequence={key} key={key} />;
-      }
-      return null;
-    case ElementTypeEnum.IMAGE:
-      return <Image src={node.url} key={key} />;
-    case ElementTypeEnum.TEXT:
-      return <RenderText node={node as TextEditorElement} key={key} />;
-  }
-}
--- app/src/components/frontside/tablet/chapterDetail/ChapterContent.tsx
+++ app/src/components/frontside/tablet/chapterDetail/ChapterContent.tsx
@@ -3,11 +3,9 @@
 import styled from "styled-components";
 
 import ChapterHeader from "@components/frontside/tablet/chapterDetail/ChapterHeader";
-import RenderAudio from "@components/frontside/tablet/chapterDetail/RenderAudio";
-import RenderText from "@components/frontside/tablet/chapterDetail/RenderText";
+import ChapterNodes from "@components/frontside/tablet/chapterDetail/ChapterNodes";
 import RequestAudioUsage from "@components/frontside/tablet/chapterDetail/RequestAudioUsage";
 import { ElementTypeEnum } from "@enums/SlateEditorEnum";
-import { MangaEditorElement, TextEditorElement } from "@interfaces/EditorInterface";
 import ChapterService from "@services/frontside/ChapterService";
 import { loadFontForNovel } from "@utils/Helpers";
 import { box } from "@utils/Themes";
@@ -37,7 +35,7 @@ export default function ChapterContent() {
     <Box>
       <ChapterHeader namePosition="Top" />
       {isAudioNodeExist ? <RequestAudioUsage /> : null}
-      <Wrap>{createContent(nodes, isAudioAccept)}</Wrap>
+      <ChapterNodes nodes={nodes} isAudioAccept={isAudioAccept} />
       <ChapterHeader />
     </Box>
   );
@@ -51,38 +49,3 @@ const Box = styled.div`
 
   margin-top: ${(props) => box(props).space.xxl};
 `;
-
-const Wrap = styled.div`
-  /* border: 1px solid red; */
-  box-sizing: border-box;
-  width: 100%;
-  margin: ${(props) => box(props).space.md} 0;
-`;
-
-const Image = styled.img`
-  /* border: 1px solid red; */
-  width: 100%;
-  display: block;
-`;
-
-function createContent(nodes: TextEditorElement[] | MangaEditorElement[], isAudioAccept: boolean) {
-  return nodes.map((node, key) => {
-    return createRenderNode(node, key, isAudioAccept);
-  });
-}
-
-function createRenderNode(node: TextEditorElement | MangaEditorElement, key: number, isAudioAccept: boolean) {
-  const type = node.type;
-
-  switch (type) {
-    case ElementTypeEnum.AUDIO:
-      if (isAudioAccept) {
-        return <RenderAudio url={node.url!} sequence={key} key={key} />;
-      }
-      return null;
-    case ElementTypeEnum.IMAGE:
-      return <Image src={node.url} key={key} />;
-    case ElementTypeEnum.TEXT:
-      return <RenderText node={node as TextEditorElement} key={key} />;
-  }
-}
