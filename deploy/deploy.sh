2dc88d7868ee7e42d4a2376a71cf03eda2ec23a0
ธีรธัช ถวิลหวัง <100046376+Teeratach-Thawilwang@users.noreply.github.com>
2024-08-24 08:40:38 +0700

    Update script.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,8 +3,9 @@
 cd /root/main/
 docker rmi $(docker images -f "dangling=true" -q)
 docker-compose stop app
-docker-compose rm app --force
+docker container prune -f
 
 DOCKER_HUB_IMAGE="teeratachdocker/luna_manga"
 docker pull $DOCKER_HUB_IMAGE
 docker-compose up -d --build
+docker-compose restart
